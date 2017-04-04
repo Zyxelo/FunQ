f5d8fd6d4be66a5ba4ae82b235cb44d230074470
Zyxelo <victorode@Victors-MBP.lan>
2017-04-04 22:38:23 +0200

    Added some basic example routes

--- src/app.js
+++ src/app.js
@@ -1,1 +1,28 @@
-var express = require(express)
+var express = require('express');
+
+var app = express();
+var port = 3000;
+
+
+
+// Require the routers - should be able to be handled dynamically
+// i.e always "require" all the routes in the routes folder
+// TODO look into this, using filesystem funcitons or something
+
+var homeRoute = require('./routes/homeRoute');
+var getUsernameRoute = require('./routes/getUserameRoute');
+
+// Use the route - Should also be able to do this dynamically
+// TODO Anton - can look at his PUM project to see how you guys did this
+app.use(homeRoute);
+app.use(getUsernameRoute);
+
+
+// Select which port to listen to
+app.listen(port, function () {
+    console.log("The application is running on localhost:" + port);
+});
+
+
+
+
---
+++ src/routes/getUserameRoute.js
@@ -0,0 +1,22 @@
+var express = require('express');
+
+var router = express.Router();
+
+// Another example of a GET request that takes a parameter from the address
+// (in this case a userID) and returns that users username
+router.get('/getUsername/:userID?', function(req,res) {
+    var userID = parseInt(req.params.userID);
+
+    if (userID === 1) {
+        res.json({username : "odelali"});
+    } else {
+        res.json({
+            username : "plankton",
+            userID : userID
+        });
+    }
+
+});
+
+// to be able to "require" the route from another file
+module.exports = router;
