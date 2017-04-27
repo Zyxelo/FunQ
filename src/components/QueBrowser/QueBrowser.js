fc1e873b5a9cd1cea14edc87bd4524c8ee6074f4
Martin Haugsbakk <marha996@student.liu.se>
2017-04-27 18:43:29 +0200

    Updated QueBrowser component

--- src/components/QueBrowser/QueBrowser.js
+++ src/components/QueBrowser/QueBrowser.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import hakan from './hakan_ullevi.jpg';
 import middag from './middag.jpg';
-import clock from './white_clock.gif';
 import sof from './sof.jpg';
 import QueElement from './QueElement/QueElement';
 import './QueBrowser.css';
@@ -10,10 +9,10 @@ class QueBrowser extends React.Component {
     render() {
         return (
             <div className="container">
-                <QueElement isTopQue={true} mainClass="top-que" imageSrc={hakan} timeLeft="7h 42min 20s" queTitle="Håkan Hellström" queDetails="Ullevi 27/6"/>
+                <QueElement isTopQue={true} mainClass="top-que" imageSrc={hakan} timeLeft="7h 42min 20s" queTitle="Håkan Hellström" queDetails="Ullevi 27/6" href=""/>
                 <div className="row">
-                    <QueElement mainClass="col-sm-12 col-md-6" imageSrc={middag} timeLeft="3h 42min 20s" queTitle="C-klassittning" queDetails="KK 5/6 18.00"/>
-                    <QueElement mainClass="col-sm-12 col-md-6" imageSrc={sof} timeLeft="14h 42min 20s" queTitle="Studentorkesterfestivalen 2017" queDetails="12/6-14/6"/>
+                    <QueElement mainClass="col-sm-12 col-md-6" imageSrc={middag} timeLeft="3h 42min 20s" queTitle="C-klassittning" queDetails="KK 5/6 18.00" href=""/>
+                    <QueElement mainClass="col-sm-12 col-md-6" imageSrc={sof} timeLeft="14h 42min 20s" queTitle="Studentorkesterfestivalen 2017" queDetails="12/6-14/6" href=""/>
                 </div>
             </div>
         );
