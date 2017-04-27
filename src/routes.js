d787525ada3643f2bb62f76db6a05377a54b6b23
Martin Haugsbakk <marha996@student.liu.se>
2017-04-27 17:00:19 +0200

    New BrowseQue component without fully using react capabilities.

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import {
 import Home from './components/Home/Home';
 import Que from './components/Que/que.component';
 import NotFound from './components/NotFound/notFound.component';
+import QueBrowser from './components/QueBrowser/QueBrowser';
 
 const Routes = () => (
     <Router>
@@ -16,6 +17,7 @@ const Routes = () => (
             <Switch>
                 <Route exact path="/" component={Home} />
                 <Route path="/que" component={Que} />
+                <Route path="/home" component={QueBrowser} />
                 <Route component={NotFound} />
             </Switch>
         </div>
