c4fefc8f65a34b6e9ef0660f68ca9b88dc950cef
Martin Haugsbakk <marha996@student.liu.se>
2017-05-03 14:38:22 +0200

    Created a create queue page

---
+++ src/components/CreateQueue/CreateQueue.js
@@ -0,0 +1,132 @@
+import React from 'react';
+import {Checkbox, Button, Col, FormGroup, FormControl, ControlLabel, Form} from 'react-bootstrap';
+
+import './CreateQueue.css';
+
+class CreateQueue extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        var today = new Date();
+        var year = today.getFullYear();
+        var month = today.getMonth()+1;
+        if (month < 10) month = '0'+month;
+        var day = today.getDate();
+        var day2 = today.getDate()+2;
+        if (day < 10) day = '0'+day;
+        if (day2 < 10) day2 = '0'+day2;
+        var startD = year+'-'+month+'-'+day;
+        var endD = year+'-'+month+'-'+day2;
+
+        this.state = {
+            openDate: startD,
+            openTime:"12:00",
+            endDate: endD,
+            endTime:"12:00",
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({[event.target.name]: event.target.value});
+    }
+
+    handleSubmit(event) {
+        alert('An essay was submitted: ' + this.state.value);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <div className="container wrapper">
+                <div className="row">
+                    <div className="col-md-2"></div>
+                    <div className="col-md-8">
+                        <Form horizontal onSubmit={this.handleSubmit}>
+
+                            <FormGroup controlId="createQueueTitle">
+                                <Col componentClass={ControlLabel} sm={3}>
+                                    Queue title
+                                </Col>
+                                <Col sm={9}>
+                                    <FormControl type="text" placeholder="Enter title" />
+                                </Col>
+                            </FormGroup>
+
+                            <FormGroup controlId="createQueueTitle">
+                                <Col componentClass={ControlLabel} sm={3}>
+                                    Queue description
+                                </Col>
+                                <Col sm={9}>
+                                    <FormControl componentClass="textarea" placeholder="Enter title" />
+                                </Col>
+                            </FormGroup>
+
+                            <FormGroup>
+                                <Col componentClass={ControlLabel} sm={3}>Queue privacy</Col>
+                                <Col sm={9}>
+                                    <Checkbox inline>
+                                        Private queue
+                                    </Checkbox>
+                                    {' '}
+                                    <Checkbox inline>
+                                        Public queue
+                                    </Checkbox>
+                                </Col>
+                            </FormGroup>
+
+                            <FormGroup>
+                                <Col componentClass={ControlLabel} sm={3}>Image link</Col>
+                                <Col sm={9}>
+                                    <FormControl type="url" placeholder="Enter image link" />
+                                </Col>
+                            </FormGroup>
+
+                            <FormGroup>
+                                <Col componentClass={ControlLabel} sm={3}>Open date</Col>
+                                <Col sm={9}>
+                                    <FormControl name="openDate" type="date" value={this.state.openDate} onChange={this.handleChange} />
+                                </Col>
+                            </FormGroup>
+
+                            <FormGroup>
+                                <Col componentClass={ControlLabel} sm={3}>Open time</Col>
+                                <Col sm={9}>
+                                    <FormControl name="openTime" type="time" value={this.state.openTime} onChange={this.handleChange} />
+                                </Col>
+                            </FormGroup>
+
+                            <FormGroup>
+                                <Col componentClass={ControlLabel} sm={3}>End date</Col>
+                                <Col sm={9}>
+                                    <FormControl name="endDate" type="date" value={this.state.endDate} onChange={this.handleChange} />
+                                </Col>
+                            </FormGroup>
+
+                            <FormGroup>
+                                <Col componentClass={ControlLabel} sm={3}>End time</Col>
+                                <Col sm={9}>
+                                    <FormControl name="endTime" type="time" value={this.state.endTime} onChange={this.handleChange} />
+                                </Col>
+                            </FormGroup>
+
+                            <FormGroup>
+                                <Col smOffset={3} sm={9}>
+                                    <Button type="submit">
+                                        Create event
+                                    </Button>
+                                </Col>
+                            </FormGroup>
+                        </Form>
+                    </div>
+                    <div className="col-md-3"></div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default CreateQueue
--- src/routes.js
+++ src/routes.js
@@ -11,6 +11,7 @@ import Navbar from './components/Navbar/QueNavbar';
 import NotFound from './components/NotFound/notFound.component';
 import QueBrowser from './components/QueBrowser/QueBrowser';
 import ReCaptcha from './components/ReCaptcha/ReCaptcha'
+import CreateQueue from './components/CreateQueue/CreateQueue';
 
 const Routes = () => (
 
@@ -22,6 +23,7 @@ const Routes = () => (
                 <Route path="/que" component={Que} />
                 <Route path="/home" component={QueBrowser} />
                 <Route path="/captcha" component={ReCaptcha}/>
+                <Route path="/create" component={CreateQueue}/>
                 <Route component={NotFound} />
             </Switch>
         </div>
